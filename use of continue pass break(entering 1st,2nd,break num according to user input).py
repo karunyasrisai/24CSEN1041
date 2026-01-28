cd00d80743205c600a7bcb13ae509c15a3fe4309
karunyasrisai <kveliche@student.gitam.edu>
2026-01-28 11:13:11 +0530

    Create use of continue pass break(entering 1st,2nd,break num according to user input).py

---
+++ use of continue pass break(entering 1st,2nd,break num according to user input).py
@@ -0,0 +1,42 @@
+first_num = int(input("Enter the first num:"))
+second_num = int(input("Enter the second num:"))
+break_num = int(input("Enter the valid num:"))
+
+for i in range(first_num, second_num):
+    if i % 2 == 0:
+        continue 
+    elif i == 133:
+        break    
+    else:
+        pass      
+
+    print(i)
+
+OUTPUT:
+Enter the first num:6
+Enter the second num:50
+Enter the valid num:43
+7
+9
+11
+13
+15
+17
+19
+21
+23
+25
+27
+29
+31
+33
+35
+37
+39
+41
+43
+45
+47
+49
+
+=== Code Execution Successful ===
