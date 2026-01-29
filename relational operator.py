c8000838fef002a8f3be61aa6e87f2bd286abf4f
karunyasrisai <kveliche@student.gitam.edu>
2026-01-29 15:36:37 +0530

    Create relational operator.py

---
+++ relational operator.py
@@ -0,0 +1,17 @@
+a = "apple"
+b = "banana"
+
+print("Relational Operators on given input:")
+print(f"{a} < {b} = {a < b}")
+print(f"{a} > {b} = {a > b}")
+print(f"{a} == {b} = {a == b}")
+print(f"{a} != {b} = {a != b}")
+
+OUTPUT:
+Relational Operators on given input:
+apple < banana = True
+apple > banana = False
+apple == banana = False
+apple != banana = True
+
+=== Code Execution Successful ===
